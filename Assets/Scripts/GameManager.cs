5d6ad0b454ee47c6b5c67c85086e2a66b52a4006
KeshavPurohit-Rogue <keshavpurohit005@gmail.com>
2025-05-20 13:42:24 +0530

    Add files via upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public int level;
+    public int lives;
+    public int score;
+    public void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        NewGame();
+    }
+
+    public void NewGame()
+    {
+        lives = 3;
+        score = 0;
+        LoadLevel(1);
+    }
+    public void LoadLevel(int index)
+    {
+        level = index;
+
+        Camera cam = Camera.main;
+        if (cam != null)
+        {
+            cam.cullingMask = 0;
+        }
+        Invoke(nameof(LoadScene), 1);
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void LevelComplete()
+    {
+        score += 1000;
+        int nextLevel = level + 1;
+
+        if (nextLevel < SceneManager.sceneCountInBuildSettings)
+        {
+            LoadLevel(nextLevel);
+        }
+        else
+        {
+            LoadLevel(1);
+        }
+    }
+
+    public void LevelFailed()
+    {
+        lives--;
+        if (lives <= 0)
+        {
+            NewGame();
+        }
+        else
+        {
+            LoadLevel(level);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,133 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] runSprites;
+    public Sprite climbSprite;
+    private int spriteIndex;
+
+    public bool climbing;
+    public bool grounded;
+
+    public Collider2D[] results;
+    public new Collider2D collider;
+
+    public float jumpStrength = 1.0f;
+    public float moveSpeed = 1f;
+
+    private Vector2 direction;
+    public new Rigidbody2D rigidbody;
+    public void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+        results = new Collider2D[4];
+    }
+
+    public void OnEnable()
+    {
+        InvokeRepeating(nameof(AnimateSprite), 1f / 12f, 1f / 12f);
+
+    }
+
+    public void OnDisable()
+    {
+        CancelInvoke();
+    }
+
+    public void CheckCollision()
+    {
+        grounded = false;
+        climbing = false;
+        Vector2 size = collider.bounds.size;
+        size.y += 0.1f;
+        size.x /= 2f;
+        int amount = Physics2D.OverlapBoxNonAlloc(transform.position, size, 0f, results);
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject hit = results[i].gameObject;
+            if (hit.layer == LayerMask.NameToLayer("Ground"))
+            {
+                grounded = hit.transform.position.y < (transform.position.y - 0.5f);
+                Physics2D.IgnoreCollision(collider, results[i], !grounded);
+            }
+            else if (hit.layer == LayerMask.NameToLayer("Ladder"))
+            {
+                climbing = true;
+            }
+        }
+    }
+    private void Update()
+    {
+        CheckCollision();
+        if (climbing)
+        {
+            direction.y = Input.GetAxis("Vertical") * moveSpeed;
+        }
+
+        else if (grounded && Input.GetButtonDown("Jump"))
+        {
+            direction = Vector2.up * jumpStrength;
+        }
+        else
+        {
+            direction = direction + Physics2D.gravity * Time.deltaTime;
+        }
+        direction.x = Input.GetAxis("Horizontal") * moveSpeed;
+
+        if (grounded)
+        {
+            direction.y = Mathf.Max(direction.y, -1.0f);
+        }
+        if (direction.x > 0)
+        {
+            transform.eulerAngles = Vector3.zero;
+        }
+        else if (direction.x < 0f)
+        {
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + direction * Time.fixedDeltaTime);
+    }
+
+    public void AnimateSprite()
+    {
+
+        if (climbing)
+        {
+            spriteRenderer.sprite = climbSprite;
+        }
+
+        else
+        {
+            spriteIndex++;
+
+            if (spriteIndex >= runSprites.Length)
+            {
+                spriteIndex = 0;
+            }
+
+            spriteRenderer.sprite = runSprites[spriteIndex];
+        }
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Objective"))
+        {
+
+            enabled = false;
+            FindObjectOfType<GameManager>().LevelComplete();
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            enabled = false;
+            FindObjectOfType<GameManager>().LevelFailed();
+        }
+    }
+}
